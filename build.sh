c271d64d7be14fdb50365b6594f967d777e00368
Auston Stewart <auston@shedbuilt.net>
2018-03-08 08:28:12 -1000

    Updating to 1.16

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 ./configure --prefix=/usr \
-            --docdir=/usr/share/doc/automake-1.15.1
-make -j $SHED_NUMJOBS
-make DESTDIR=${SHED_FAKEROOT} install
+            --docdir=/usr/share/doc/automake-1.16 && \
+make -j $SHED_NUMJOBS && \
+make DESTDIR="$SHED_FAKEROOT" install
