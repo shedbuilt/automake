a811596fecd2a3421a8490c28f64ec4b95b0a426
Auston Stewart <auston@shedbuilt.net>
2018-05-30 09:03:43 -1000

    Updating for blank

--- build.sh
+++ build.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
+declare -A SHED_PKG_LOCAL_OPTIONS=${SHED_PKG_OPTIONS_ASSOC}
+SHED_PKG_LOCAL_DOCDIR="/usr/share/doc/${SHED_PKG_NAME}-${SHED_PKG_VERSION}"
+# Configure
 ./configure --prefix=/usr \
-            --docdir=/usr/share/doc/automake-1.16.1 &&
+            --docdir="$SHED_PKG_LOCAL_DOCDIR" &&
+# Build and Install
 make -j $SHED_NUM_JOBS &&
-make DESTDIR="$SHED_FAKE_ROOT" install
+make DESTDIR="$SHED_FAKE_ROOT" install || exit 1
+# Prune Documentation
+if [ -z "${SHED_PKG_LOCAL_OPTIONS[docs]}" ]; then
+    rm -rf "${SHED_FAKE_ROOT}/usr/share/doc"
+fi
